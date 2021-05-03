51280730103dc65d1e9071120f1d736c63edf0cd
Alfredo Fernandez <alfredo@hotmail.com>
2021-05-03 07:19:47 -0400

    make import from Header in layouts

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,16 @@
 <template>
   <div class="min-h-screen">
-    <TheHeader />
+    <!-- <UiTheHeader /> -->
+    <Header />
     <main class="pt-4 container px-3">
       <nuxt />
     </main>
     <UiTheFooter />
   </div>
 </template>
+<script>
+import Header from '@/components/ui/TheHeader'
+export default {
+  components: { Header },
+}
+</script>
